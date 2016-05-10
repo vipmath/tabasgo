e06f21b7abce7a9ab7a577cb919e490f4fd3bf49
Jean-Francois Romang <jromang@posteo.de>
2016-05-10 18:38:23 +0200

    Create board class

---
+++ board.h
@@ -0,0 +1,31 @@
+/*
+  TabasGo, a GTP compatible Go playing engine.
+  Copyright (C) 2016 Jean-Francois Romang
+
+  TabasGo is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  TabasGo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with TabasGo.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TABASGO_BOARD_H
+#define TABASGO_BOARD_H
+
+#include <cstdint>
+
+class Board {
+
+private:
+    uint8_t board[];
+};
+
+
+#endif //TABASGO_BOARD_H
